272e42b2ff7390188e4c8e87477ff6a6c1295b3a
Andrew Fisher <fisher@cognitoforms.com>
2022-08-11 17:12:00 -0400

    Tweak JS

--- save-button/p/scripts.js
+++ save-button/p/scripts.js
@@ -2,9 +2,14 @@ var code = window.innerWidth + 'X' + window.innerHeight;
 
 document.body.addEventListener('click', function(e){
   code = code + 'C' + e.x + 'X' + e.y;
+  alert('Thank you. Your code is: ' + code + 'T' + Math.round(e.timeStamp));
+  code = code + '-';
 });
 
 document.getElementById('save').addEventListener('click', function(e){
-  code = code + 'T' + Math.round(e.timeStamp);
+  e.stopPropagation();
+  
+  code = code + 'CST' + Math.round(e.timeStamp);
   alert('Thank you. Your code is: ' + code);
+  code = code + '-';
 });
