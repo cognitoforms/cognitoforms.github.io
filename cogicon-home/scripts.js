504bcdcab7a2be77932e385d118319426a8a35f8
Andrew Fisher <fisher@cognitoforms.com>
2023-04-03 16:27:34 -0400

    Add cogicon test

---
+++ cogicon-home/scripts.js
@@ -0,0 +1,15 @@
+var code = window.innerWidth + 'X' + window.innerHeight;
+
+document.body.addEventListener('click', function(e){
+  code = code + 'C' + e.x + 'X' + e.y;
+  alert('Thank you. Your code is: ' + code + 'T' + Math.round(e.timeStamp));
+  code = code + '-';
+});
+
+document.getElementById('cogicon').addEventListener('click', function(e){
+  e.stopPropagation();
+  
+  code = code + 'CST' + Math.round(e.timeStamp);
+  alert('Thank you. Your code is: ' + code);
+  code = code + '-';
+});
