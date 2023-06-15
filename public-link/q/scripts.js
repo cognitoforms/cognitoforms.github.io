60145186aff1a6d181f1cafcf0624c3f1af613cc
Alex Hutton <amhutton@email.sc.edu>
2023-06-15 15:52:17 -0400

    'q' now gives a code with coordinates & time

--- public-link/q/scripts.js
+++ public-link/q/scripts.js
@@ -3,18 +3,20 @@ var startTime = Date.now();
 function handleClick(event) {
     var image = event.target;
     var rect = image.getBoundingClientRect();
-    var naturalWidth = image.naturalWidth;
-    var naturalHeight = image.naturalHeight;
-    var xPos = (event.clientX - rect.left) * (naturalWidth / rect.width);
-    var yPos = (event.clientY - rect.top) * (naturalHeight / rect.height);
+    var xPos = Math.floor((event.clientX - rect.left) * (image.naturalWidth / rect.width));
+    var yPos = Math.floor((event.clientY - rect.top) * (image.naturalHeight / rect.height));
 
-    var endTime = Date.now();
-    var elapsedTime = (endTime - startTime) / 1000;
+    var elapsedTime = (Date.now() - startTime) / 1000;
 
-    if (xPos >= 0 && xPos <= 800 && yPos >= 1000 && yPos <= 1080) {
-        alert('Public Links Was Selected \nClicked at coordinates: X = ' + xPos + ', Y = ' + yPos + '\nTotal time spent: ' + elapsedTime + ' seconds');
-    } else
-        alert('Public Links Was Not Selected \nClicked at coordinates: X = ' + xPos + ', Y = ' + yPos + '\nTotal time spent: ' + elapsedTime + ' seconds');
+    var code = xPos + 'C' + yPos + 'T' + elapsedTime;
+    alert('Thank you, your code is: ' + code);
+
+    /* We can infer correctness when we parse the data
+        if (xPos >= 0 && xPos <= 800 && yPos >= 1000 && yPos <= 1080) {
+            alert('Thank you, your code is: ' + code + 'A');
+        } else
+            alert('Thank you, your code is: ' + code + 'B');
+    */
 }
 
 document.addEventListener('DOMContentLoaded', function () {
