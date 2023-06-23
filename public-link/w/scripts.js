f1a179528a83465583f0489786e879a1d65610ad
Alex Hutton <amhutton@email.sc.edu>
2023-06-23 10:45:51 -0400

    Added modal to w & z tests

--- public-link/w/scripts.js
+++ public-link/w/scripts.js
@@ -7,12 +7,19 @@ function handleClick(event) {
     var xPos = Math.floor((event.clientX - rect.left) * (image.naturalWidth / rect.width));
     var yPos = Math.floor((event.clientY - rect.top) * (image.naturalHeight / rect.height));
 
+    var modal = document.getElementById('modal');
+    var modalContent = document.getElementById('modal-content');
+
     if (xPos >= 0 && xPos <= 800 && yPos >= 1000 && yPos <= 1080) { // Public Links
-        alert('Thank you, your code is: ' + code + 'CST' + Math.round((Date.now() - startTime) / 1000));
+        modalContent.textContent = 'Thank you, your code is: ' + code + 'CST' + Math.round((Date.now() - startTime) / 1000);
     } else { // Anywhere Else
-        alert('Thank you, your code is: ' + code + 'C' + xPos + 'X' + yPos + "T" + Math.round((Date.now() - startTime) / 1000));
+        modalContent.textContent = 'Thank you, your code is: ' + code + 'C' + xPos + 'X' + yPos + "T" + Math.round((Date.now() - startTime) / 1000);
     }
 
+    modal.style.display = 'block';
+    image.style.filter = 'blur(5px)';
+    image.style.opacity = '0.5';
+    image.style.pointerEvents = 'none';
 }
 
 document.addEventListener('DOMContentLoaded', function () {
--- public-link/z/scripts.js
+++ public-link/z/scripts.js
@@ -7,14 +7,21 @@ function handleClick(event) {
     var xPos = Math.floor((event.clientX - rect.left) * (image.naturalWidth / rect.width));
     var yPos = Math.floor((event.clientY - rect.top) * (image.naturalHeight / rect.height));
 
+    var modal = document.getElementById('modal');
+    var modalContent = document.getElementById('modal-content');
+
 
     if (xPos <= 210 && yPos >= 133 && yPos <= 220) { // CSS - Settings is selected
-        alert('Thank you, your code is: ' + code + 'CSS' + Math.round((Date.now() - startTime) / 1000));
+        modalContent.textContent = 'Thank you, your code is: ' + code + 'CSS' + Math.round((Date.now() - startTime) / 1000);
     } else if (xPos >= 435 && xPos <= 667 && yPos >= 133 && yPos <= 220) { // CSW - Workflow is selected
-        alert('Thank you, your code is: ' + code + 'CSW' + Math.round((Date.now() - startTime) / 1000));
+        modalContent.textContent = 'Thank you, your code is: ' + code + 'CSW' + Math.round((Date.now() - startTime) / 1000);
     } else //Neither were selected
-        alert('Thank you, your code is: ' + code + 'C' + xPos + 'X' + yPos + "T" + Math.round((Date.now() - startTime) / 1000));
+        modalContent.textContent = 'Thank you, your code is: ' + code + 'C' + xPos + 'X' + yPos + "T" + Math.round((Date.now() - startTime) / 1000);
 
+    modal.style.display = 'block';
+    image.style.filter = 'blur(5px)';
+    image.style.opacity = '0.5';
+    image.style.pointerEvents = 'none';
 }
 
 document.addEventListener('DOMContentLoaded', function () {
