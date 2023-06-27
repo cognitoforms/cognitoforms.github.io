4531dc3dda79405d7b04059c5439a7ba1afdcbba
Alex Hutton <amhutton@email.sc.edu>
2023-06-27 11:02:22 -0400

    Survey capabilities added

--- public-link/w/scripts.js
+++ public-link/w/scripts.js
@@ -10,19 +10,44 @@ function handleClick(event) {
     var modal = document.getElementById('modal');
     var modalContent = document.getElementById('modal-content');
 
-    if (xPos >= 0 && xPos <= 800 && yPos >= 1000 && yPos <= 1080) { // Public Links
-        modalContent.textContent = 'Thank you, your code is: ' + code + 'CST' + (Date.now() - startTime) / 1000;
+    if (xPos <= 400 && yPos >= 430 && yPos <= 540) { // Public Links
+        code = 'Thank you, your code is: ' + code + 'CST' + (Date.now() - startTime) / 1000;
     } else { // Anywhere Else
-        modalContent.textContent = 'Thank you, your code is: ' + code + 'C' + xPos + 'X' + yPos + "T" + (Date.now() - startTime) / 1000;
+        code = 'Thank you, your code is: ' + code + 'C' + xPos + 'X' + yPos + 'T' + (Date.now() - startTime) / 1000;
     }
+
 
     modal.style.display = 'block';
     image.style.filter = 'blur(5px)';
     image.style.opacity = '0.5';
     image.style.pointerEvents = 'none';
+
+    var button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
+    var button3 = document.getElementById("button3");
+    var button4 = document.getElementById("button4");
+
+    button1.addEventListener("click", function () {
+        modalContent.textContent = code + 'A';
+        modalContent.style.marginTop = '52px';
+    });
+    button2.addEventListener("click", function () {
+        modalContent.textContent = code + 'B';
+        modalContent.style.marginTop = '52px';
+    });
+    button3.addEventListener("click", function () {
+        modalContent.textContent = code + 'C';
+        modalContent.style.marginTop = '52px';
+    });
+    button4.addEventListener("click", function () {
+        modalContent.textContent = code + 'D';
+        modalContent.style.marginTop = '52px';
+    });
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     var settingsImage = document.getElementById('settings');
     settingsImage.addEventListener('click', handleClick);
 });
+
